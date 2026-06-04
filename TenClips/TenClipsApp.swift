ef6051126808d332b46b73005fe28adb721ea5a2
Wanglin He <h894734566@163.com>
2026-06-04 20:38:52 +0800

    Initial Commit

---
+++ TenClips/TenClipsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TenClipsApp.swift
+//  TenClips
+//
+//  Created by 何旺霖 on 2026/6/4.
+//
+
+import SwiftUI
+
+@main
+struct TenClipsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
